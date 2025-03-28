a93313740312a3577c803da47d94e3625b0dc652
unknown <adityavikramsinha19@gmail.com>
2025-03-28 11:58:30 +0530

    weird

--- src/datastructures/trie/Trie.java
+++ src/datastructures/trie/Trie.java
@@ -4,31 +4,59 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.HashMap;
 
-
+/**
+ * Classic implementation of a Trie, that exposes the root so that it can be extended
+ * later on for more algorithm implementations.
+ *
+ */
 public class Trie {
-    private static class Node {
-        char data;
-        HashMap<Character, Node> children = new HashMap<>();
+
+    /**
+     * Node for the Trie
+     */
+    public static class Node {
+        public int data;
+        public HashMap<Integer, Node> children = new HashMap<>();
         private boolean eow = false;
 
-        Node(char c) {
+
+        /**
+         * Constructs a {@code Node} with the given int as data
+         * @param c the data of the node
+         */
+        Node(int c) {
             data = c;
         }
 
-        Node addChild(char c) {
+        /**
+         * Constructs a new {@code Node} with the given data
+         * @param c the data of the new Node
+         * @return a reference to the new Node
+         */
+        @NotNull Node addChild( int c) {
             Node child= new Node(c);
             children.put(c, child);
             return child;
         }
 
+        /**
+         * Sets node as End of Word
+         */
         void setEOW() {
             this.eow = true;
         }
 
+        /**
+         * @return whether node is End of Word
+         */
         boolean isEOW() {
             return eow;
         }
 
+        /**
+         * Leaf implies that node has no children left to explore
+         * @return whether the node is a leaf or not.
+         */
         boolean isLeaf() {
             return children.isEmpty();
         }
@@ -36,12 +64,17 @@ public class Trie {
 
     private final Node root = new Node('\0');
 
+    /**
+     * Inserts a new String s, into the Trie
+     * @param s the String to insert
+     * @return {@code true} if a new word was inserted, {@code false} if it already existed as a word
+     */
     public boolean insert(@NotNull String s) {
         Node n = root;
         int ptr = 0;
 
-        while (ptr < s.length() && !n.isLeaf() && n.children.containsKey(s.charAt(ptr))) {
-            n = n.children.get(s.charAt(ptr++));
+        while (ptr < s.length() && !n.isLeaf() && n.children.containsKey((int)s.charAt(ptr))) {
+            n = n.children.get((int)s.charAt(ptr++));
         }
 
         if (ptr == s.length() && n.isLeaf() && n.isEOW()) {
@@ -59,16 +92,10 @@ public class Trie {
         return true;
     }
 
-    public boolean contains(String s) {
-        Node n = root;
-        int ptr = 0;
-        while (ptr < s.length()) {
-            if (n.children.containsKey(s.charAt(ptr))) {
-                n = n.children.get(s.charAt(ptr++));
-            } else
-                return false;
-        }
-
-        return n.isEOW();
+    /**
+     * @return  a reference to the root that is a {@code final} data member of the class a reference to the root that is a {@code final} data member of the class
+     */
+    public Node getRoot() {
+        return root;
     }
 }
